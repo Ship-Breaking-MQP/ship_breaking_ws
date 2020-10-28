3e04020ddd8c60d80380c0c0497af410f8b00410
Noble Kalish <noblekalish1220@gmail.com>
2020-10-28 15:22:53 -0400

    boiler plate stuff

--- mqp_ws/src/scrap_burning/src/plug_in_controller.cpp
+++ mqp_ws/src/scrap_burning/src/plug_in_controller.cpp
@@ -1,4 +1,34 @@
 //
 // Created by nkalish on 10/28/20.
 //
+#include "plug_in_controller.h"
 
+#include <cmath>
+
+#include <controller_interface/controller_base.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+
+#include <franka/robot_state.h>
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+
+bool scrap_burning_controllers::PlugInController::init(hardware_interface::RobotHW *robot_hw, ros::NodeHandle &node_handle) {
+    return true;
+}
+
+void scrap_burning_controllers::PlugInController::starting(const ros::Time &) {
+
+}
+
+void scrap_burning_controllers::PlugInController::update(const ros::Time &, const ros::Duration &period) {
+
+}
+
+//void scrap_burning_controllers::PlugInController::updateDynamicReconfigure() {
+//
+//}
+//
+//void scrap_burning_controllers::PlugInController::plugInParamCallback(scrap_burning::plug_in_paramConfig &config, uint32_t level) {
+//
+//}
--- mqp_ws/src/scrap_burning/src/plug_in_controller.h
+++ mqp_ws/src/scrap_burning/src/plug_in_controller.h
@@ -1,8 +1,38 @@
 //
 // Created by nkalish on 10/28/20.
 //
+#pragma once
 
-#ifndef SRC_PLUG_IN_CONTROLLER_H
-#define SRC_PLUG_IN_CONTROLLER_H
+#include <memory>
+#include <string>
+#include <vector>
 
-#endif //SRC_PLUG_IN_CONTROLLER_H
+#include <controller_interface/multi_interface_controller.h>
+#include <dynamic_reconfigure/server.h>
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+#include <Eigen/Dense>
+
+namespace scrap_burning_controllers {
+
+    class PlugInController : public controller_interface::MultiInterfaceController<
+            franka_hw::FrankaModelInterface,
+            hardware_interface::EffortJointInterface,
+            franka_hw::FrankaStateInterface> {
+
+    public:
+        bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) override;
+        void starting(const ros::Time&) override;
+        void update(const ros::Time&, const ros::Duration& period) override;
+
+    private:
+        std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+        std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
+        std::vector<hardware_interface::JointHandle> joint_handles_;
+        
+    };
+}
